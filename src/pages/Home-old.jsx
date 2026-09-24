216229a616e15de63a5bed95909e8fa42438e997
Nattapol12 <dajaroen_n@silpakorn.edu>
2026-09-24 05:27:51 +0000

    api

--- src/pages/Home-old.jsx
+++ src/pages/Home-old.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import FeaturedCarousel from '../components/FeaturedCarousel';
-import { getMovies } from '../api/tmdb';
-//import { movies as localMovies } from '../data/data';
+import { movies as localMovies } from '../data/data';
 // TODO ขั้นที่ 5: import { useEffect } from 'react' และ import { getMovies } from '../api/tmdb'
 
 const STEPS = [
@@ -25,18 +24,10 @@ function shuffle(list) {
 }
 
 function Home() {
-    const [picks, setPicks] = useState([]);
   // สุ่มครั้งเดียวตอน component เกิด แล้วจำไว้ใน state (กดเลื่อนแล้วลำดับไม่เปลี่ยน)
   // TODO ขั้นที่ 5: เปลี่ยนเป็น useState([]) แล้วใช้ useEffect เรียก getMovies() แล้ว setPicks(shuffle(list))
-  //const [picks, setPicks] = useState(() => shuffle(localMovies));
-    useEffect(() => {
-    let ignore = false;
-    getMovies()
-      .then(list => { if (!ignore) setPicks(shuffle(list)); })
-      .catch(() => { if (!ignore) setPicks([]); });   // พลาดก็แค่ไม่มีหนังแนะนำ หน้าแรกไม่ควรพัง
-    return () => { ignore = true; };
-  }, []);
-  
+  const [picks, setPicks] = useState(() => shuffle(localMovies));
+
   return (
     <div className="mx-auto max-w-5xl px-4 md:px-6">
       {/* Hero */}
